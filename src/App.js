06853da269ec5dda76398cc9b46b312f716dee2e
UrosJavaScript <kowcicuros70@gmail.com>
2024-07-17 17:18:36 +0200

    adding API call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavBar } from "./components/nav";
 import { Main } from "./components/main";
-import { tempMovieData } from "./helpers/tempMovieData";
+// import { tempMovieData } from "./helpers/tempMovieData";
 import { Search } from "./components/search";
 import { NumResults } from "./components/numResults";
 import { Box } from "./components/box";
 import { MovieList } from "./components/movieList";
-import { tempWatchedData } from "./helpers/tempWatchedData";
+// import { tempWatchedData } from "./helpers/tempWatchedData";
 import { WatchedSummary } from "./components/watchedSummary";
 import { WatchedMoviesList } from "./components/watchedList";
+import { Loader } from "./components/loader";
+
+const KEY = "97e6c7f0";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const query = "interstellar";
+
+  useEffect(function () {
+    async function fetchMovies() {
+      setIsLoading(true);
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    }
+
+    fetchMovies();
+  }, []);
+
+  console.log("movies: ", movies);
+
   return (
     <>
       <NavBar>
@@ -20,9 +42,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StarRating } from "./components/starRating";
-// import "./index.css";
-// import App from "./App";
+// import { StarRating } from "./components/starRating";
+import "./index.css";
+import App from "./App";
 
 const id = document.getElementById("root");
 const root = ReactDOM.createRoot(id);
 
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating color="red" size={20} className="test" defaultRating={3} />
+    <StarRating color="red" size={20} className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
