22e2ce94b722d960a16ea09a045ae14fbc642da1
UrosJavaScript <kowcicuros70@gmail.com>
2024-07-23 12:38:16 +0200

    correction of a minor error

--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -56,7 +56,7 @@ export const MovieDetails = ({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
 
         const data = await res.json();
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -16,7 +16,7 @@ export const useMovies = (query) => {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
